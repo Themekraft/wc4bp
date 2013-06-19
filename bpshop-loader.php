aed8d7311f4f7a3f201814647f88b3cbb3a44daf
Sven Lehnert <mail@sven-lehnert.de>
2013-06-19 14:03:39 +0200

    Version 1.0.9

--- bpshop-loader.php
+++ bpshop-loader.php
@@ -4,7 +4,7 @@
  * Plugin URI:  https://github.com/Themekraft/WooCommerce-for-Buddypress
  * Description: Integrates a WooCommerce installation with a BuddyPress social network
  * Author:      BP Shop Dev Team
- * Version:     1.0.8
+ * Version:     1.0.9
  * Author URI:  https://github.com/Themekraft/WooCommerce-for-Buddypress
  * Network:		true
  *
@@ -35,7 +35,7 @@ class BPSHOP_Loader
 	/**
 	 * The plugin version
 	 */
-	const VERSION 	= '1.0.8';
+	const VERSION 	= '1.0.9';
 
 	/**
 	 * Minimum required WP version
