a04fa1d58067652f03282ed96cefbebd3f8c6de6
Sven Lehnert <mail@sven-lehnert.de>
2013-12-17 13:33:01 +0100

    = v1.3.2 = * fixed a small bug in the cart url after "add to cart" the "View Cart" link was broken.

--- wc4bp-basic-integration.php
+++ wc4bp-basic-integration.php
@@ -4,7 +4,7 @@
  * Plugin URI:  http://themekraft.com/store/woocommerce-buddypress-integration-wordpress-plugin/
  * Description: Integrates a WooCommerce installation with a BuddyPress social network
  * Author:      WC4BP Integration Dev Team ;)
- * Version:     1.3
+ * Version:     1.3.2
  *
  *****************************************************************************
  *
@@ -37,7 +37,7 @@ class WC4BP_Loader {
 	/**
 	 * The plugin version
 	 */
-	const VERSION 	= '1.3';
+	const VERSION 	= '1.3.2';
 
 	/**
 	 * Minimum required WP version
@@ -74,7 +74,7 @@ class WC4BP_Loader {
 	/**
 	 * @var string
 	 */
-	public $version = '1.3';
+	public $version = '1.3.2';
 
 	/**
 	 * @var string
