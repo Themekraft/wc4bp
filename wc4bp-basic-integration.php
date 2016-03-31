f5a1917af15a32008f1cbe453aa82581fc190a05
Sven Lehnert <mail@sven-lehnert.de>
2016-03-31 09:36:29 +0200

    fixed css props to tristanpenman for the pull request ;)

--- wc4bp-basic-integration.php
+++ wc4bp-basic-integration.php
@@ -4,7 +4,7 @@
  * Plugin URI:  http://themekraft.com/store/woocommerce-buddypress-integration-wordpress-plugin/
  * Description: Integrates a WooCommerce installation with a BuddyPress social network
  * Author:      WC4BP Integration Dev Team ;)
- * Version:     2.3.4
+ * Version:     2.3.5
  *
  *****************************************************************************
  *
@@ -37,7 +37,7 @@ class WC4BP_Loader {
 	/**
 	 * The plugin version
 	 */
-	const VERSION 	= '2.3.4';
+	const VERSION 	= '2.3.5';
 
     /**
 	 * Minimum required WP version
