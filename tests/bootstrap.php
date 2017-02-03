025c76010617ab0e6d961bb2ca58af9358ce8360
gfirem@gmail.com <gfirem@gmail.com>
2017-02-03 15:54:35 -0400

    Updating travis config

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -7,8 +7,10 @@
 
 $_tests_dir = getenv( 'WP_TESTS_DIR' );
 if ( ! $_tests_dir ) {
-	$_tests_dir = '/tmp/wordpress-tests-lib';
+	$_tests_dir = '/tmp/wordpress/test/phpunit';
 }
+
+echo "TestDir".$_tests_dir."<br/>";
 
 // Give access to tests_add_filter() function.
 require_once $_tests_dir . '/includes/functions.php';
