d7a454e8ecfa6d431585b09a33b392185103a03a
gfirem@gmail.com <gfirem@gmail.com>
2017-03-22 10:10:19 -0400

    Fixing the disable my account tabs in the free version. https://github.com/Themekraft/wc4bp/issues/59

--- admin/views/main/html_admin_my_account_tabs.php
+++ admin/views/main/html_admin_my_account_tabs.php
@@ -6,7 +6,8 @@ foreach ( WC4BP_MyAccount::get_available_endpoints() as $end_point_key => $end_p
 	if ( isset( $wc4bp_options[ 'wc4bp_endpoint_' . $end_point_key ] ) ) {
 		$tab_select = $wc4bp_options[ 'wc4bp_endpoint_' . $end_point_key ];
 	}
-	echo "<p " . $this->disable_class_tag( 'p' ) . "><input " . $this->disable_input_tag( 'checkbox' ) . " name='wc4bp_options[wc4bp_endpoint_" . $end_point_key . "]' type='checkbox' value='1' " . checked( $tab_select, 1, false ) . " /> <b>Turn off '" . $end_point_name . "' tab. </b></p>";
+	$text = ( $this->is_free ) ? sprintf( 'Turn on %s tab.', $end_point_name ) : sprintf( 'Turn off %s tab.', $end_point_name );
+	echo "<p " . $this->disable_class_tag( 'p' ) . "><input " . $this->disable_input_tag( 'checkbox' ) . " name='wc4bp_options[wc4bp_endpoint_" . $end_point_key . "]' type='checkbox' value='1' " . checked( $tab_select, 1, false ) . " /> <b>" . $text . "</b></p>";
 }
 
 
