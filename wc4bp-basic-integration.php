86263faaa79f4e968e3292801cab0c8cc5e765f9
gfirem@gmail.com <gfirem@gmail.com>
2017-03-28 12:53:31 -0400

    Refactoring the code. Integrating EDD with freemius.

--- wc4bp-basic-integration.php
+++ wc4bp-basic-integration.php
@@ -78,8 +78,8 @@ class WC4BP_Loader {
 	public function __construct() {
 		self::$plugin_name = plugin_basename( __FILE__ );
 		$this->constants();
-		require_once dirname( __FILE__ ) . '/class/inlcudes/WP_Requirements.php';
-		require_once dirname( __FILE__ ) . '/class/inlcudes/class-tgm-plugin-activation.php';
+		require_once dirname( __FILE__ ) . '/class/includes/WP_Requirements.php';
+		require_once dirname( __FILE__ ) . '/class/includes/class-tgm-plugin-activation.php';
 		require_once dirname( __FILE__ ) . '/class/wc4bp-base.php';
 		require_once dirname( __FILE__ ) . '/class/wc4bp-manager.php';
 		require_once dirname( __FILE__ ) . '/class/wc4bp-required-php.php';
@@ -91,6 +91,8 @@ class WC4BP_Loader {
 			if ( wc4bp_Manager::is_woocommerce_active() && wc4bp_Manager::is_buddypress_active() ) {
 				// Init Freemius.
 				self::$freemius = $this->wc4bp_fs();
+				//Adding edd migration code
+				require_once WC4BP_ABSPATH_CLASS_PATH . 'includes/client-migration/edd.php';
 				
 				new wc4bp_Manager();
 				
@@ -119,7 +121,7 @@ class WC4BP_Loader {
 		
 		if ( ! isset( $wc4bp_fs ) ) {
 			// Include Freemius SDK.
-			require_once WC4BP_ABSPATH_ADMIN_PATH . 'resources/freemius/start.php';
+			require_once WC4BP_ABSPATH_CLASS_PATH . 'includes/freemius/start.php';
 			
 			$wc4bp_fs = fs_dynamic_init( array(
 				'id'                  => '425',
@@ -149,10 +151,10 @@ class WC4BP_Loader {
 	private function constants() {
 		define( 'WC4BP_VERSION', self::VERSION );
 		define( 'WC4BP_FOLDER', plugin_basename( dirname( __FILE__ ) ) );
-		define( 'WC4BP_ABSPATH', trailingslashit( str_replace( "\\", "/", WP_PLUGIN_DIR . '/' . WC4BP_FOLDER ) ) );
+		define( 'WC4BP_ABSPATH', dirname( __FILE__ ) . DIRECTORY_SEPARATOR );
 		define( 'WC4BP_URLPATH', trailingslashit( plugins_url( '/' . WC4BP_FOLDER ) ) );
-		define( 'WC4BP_ABSPATH_CLASS_PATH', WC4BP_ABSPATH . 'class/' );
-		define( 'WC4BP_ABSPATH_TEMPLATE_PATH', WC4BP_ABSPATH . 'templates/' );
+		define( 'WC4BP_ABSPATH_CLASS_PATH', dirname( __FILE__ ) . DIRECTORY_SEPARATOR . 'class' . DIRECTORY_SEPARATOR );
+		define( 'WC4BP_ABSPATH_TEMPLATE_PATH', WC4BP_ABSPATH . 'templates' . DIRECTORY_SEPARATOR );
 		define( 'WC4BP_ABSPATH_ADMIN_PATH', dirname( __FILE__ ) . DIRECTORY_SEPARATOR . 'admin' . DIRECTORY_SEPARATOR );
 		define( 'WC4BP_ABSPATH_ADMIN_VIEWS_PATH', dirname( __FILE__ ) . DIRECTORY_SEPARATOR . 'admin' . DIRECTORY_SEPARATOR . 'views' . DIRECTORY_SEPARATOR );
 		define( 'WC4BP_CSS', WC4BP_URLPATH . '/admin/css/' );
