63d83416acdc32f7baa9d0cc4d948097fb099d2a
Victor Marin <marin250189@gmail.com>
2017-05-30 23:46:35 -0400

    I have WC4BP premium, but since updating to Woocommerce 3.0 about a week ago, the BP fields on my checkout process have disappeared and I'm unable to add them again. Also, when I try to "Sync Profile Fields", I get a returned value of 0.

--- admin/admin-ajax.php
+++ admin/admin-ajax.php
@@ -25,6 +25,9 @@ class wc4bp_admin_ajax extends wc4bp_base {
 		add_action( 'wp_ajax_wc4bp_delete_page', array( $this, 'wc4bp_delete_page' ) );
 		add_action( 'wp_ajax_nopriv_wc4bp_delete_page', array( $this, 'wc4bp_delete_page' ) );
 
+       add_action( 'wp_ajax_wc4bp_shop_profile_sync_ajax', array( $this, 'wc4bp_shop_profile_sync_ajax' ) );
+       add_action( 'wp_ajax_nopriv_wc4bp_shop_profile_sync_ajax', array( $this, 'wc4bp_shop_profile_sync_ajax' ) );
+
 //		add_action( 'wp_ajax_wc4bp_thickbox_add_page', 'wc4bp_thickbox_add_page' );
 //		add_action( 'wp_ajax_nopriv_wc4bp_thickbox_add_page', 'wc4bp_thickbox_add_page' );
 	}
@@ -47,7 +50,66 @@ class wc4bp_admin_ajax extends wc4bp_base {
 		wc4bp_admin_pages::wc4bp_add_edit_entry_form_call( 'edit' );
 		die();
 	}
-	
+    public function wc4bp_shop_profile_sync_ajax() {
+        $update_type = sanitize_text_field( $_POST['update_type'] );
+
+        $number = 20;
+        $paged  = isset( $_POST['wc4bp_page'] ) ? intval( sanitize_text_field( $_POST['wc4bp_page'] ) ) : 1;
+        $offset = ( $paged - 1 ) * $number;
+        $query  = get_users( '&offset=' . $offset . '&number=' . $number );
+
+        include_once( WC4BP_ABSPATH_ADMIN_VIEWS_PATH . 'sync/html_admin_sync_shop_profile_sync_ajax.php' );
+    }
+    public function wc4bp_change_xprofile_visibility_by_user_ajax( $user_id ) {
+        // get the corresponding  wc4bp fields
+        $shipping         = bp_get_option( 'wc4bp_shipping_address_ids' );
+        $billing          = bp_get_option( 'wc4bp_billing_address_ids' );
+        $visibility_level = sanitize_text_field( $_POST['visibility_level'] );
+
+        foreach ( $shipping as $key => $field_id ) {
+            xprofile_set_field_visibility_level( $field_id, $user_id, $visibility_level );
+        }
+        foreach ( $billing as $key => $field_id ) {
+            xprofile_set_field_visibility_level( $field_id, $user_id, $visibility_level );
+        }
+    }
+    public function wc4bp_sync_from_admin( $user_id ) {
+        // get the profile fields
+        $shipping = bp_get_option( 'wc4bp_shipping_address_ids' );
+        $billing  = bp_get_option( 'wc4bp_billing_address_ids' );
+
+        $groups = BP_XProfile_Group::get( array(
+            'fetch_fields' => true
+        ) );
+
+        if ( ! empty( $groups ) ) {
+            foreach ( $groups as $group ) {
+                if ( empty( $group->fields ) ) {
+                    continue;
+                }
+
+                foreach ( $group->fields as $field ) {
+
+                    $billing_key  = array_search( $field->id, $billing );
+                    $shipping_key = array_search( $field->id, $shipping );
+
+                    if ( $shipping_key ) {
+                        $type       = 'shipping';
+                        $field_slug = $shipping_key;
+                    }
+
+                    if ( $billing_key ) {
+                        $type       = 'billing';
+                        $field_slug = $billing_key;
+                    }
+
+                    if ( isset( $field_slug ) ) {
+                        xprofile_set_field_data( $field->id, $user_id, get_user_meta( $user_id, $type . '_' . $field_slug, true ) );
+                    }
+                }
+            }
+        }
+    }
 	
 	public function wc4bp_add_page( $wc4bp_page_id ) {
 		
