23f56ae3d88da85cd20b26fe148958eed7377a98
Victor Marin <marin250189@gmail.com>
2017-08-03 21:20:41 -0400

    when working with the free version, put the option 'Select the tab you want to use as your Shop Home' disabled

--- admin/views/main/html_admin_shop_home.php
+++ admin/views/main/html_admin_shop_home.php
@@ -1,6 +1,6 @@
 <p><?php _e( 'Select the tab you want to use as your Shop Home.', 'wc4bp' ); ?></p>
-
-<select name='wc4bp_options[tab_shop_default]'>
+<p <?php echo $this->disable_class_tag( 'p', wc4bp_base::$starter_plan_id ); ?>></p>
+<select <?php echo $this->disable_input_tag( 'checkbox', wc4bp_base::$starter_plan_id ); ?>  name='wc4bp_options[tab_shop_default]'>
 	<?php
 	if ( isset( $wc4bp_pages_options['selected_pages'] ) && is_array( $wc4bp_pages_options['selected_pages'] ) && count( $wc4bp_pages_options['selected_pages'] ) > 0 ) {
 		echo '<option value="default" ' . selected( $wc4bp_options['tab_shop_default'], 'default', false ) . '>Default</option>';
