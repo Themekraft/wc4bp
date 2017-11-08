d3b5a5810fac10dbc726bebcf492894a4bb85aee
Guillermo Figueroa Mesa <gfirem@gmail.com>
2017-11-08 01:42:01 -0300

    Cleaning and refactoring the code

--- class/wc4bp-woocommerce.php
+++ class/wc4bp-woocommerce.php
@@ -24,7 +24,6 @@ class wc4bp_Woocommerce {
 			add_filter( 'woocommerce_is_account_page', array( $this, 'wc4bp_woocommerce_is_account_page__premium_only' ) );
 		}
 		add_filter( 'woocommerce_get_endpoint_url', array( $this, 'endpoint_url' ), 1, 4 );
-		add_filter( 'wcs_get_view_subscription_url', array( $this, 'wc4bp_get_view_subscription_url' ), 1, 2 );
 		add_filter( 'woocommerce_available_payment_gateways', array( $this, 'available_payment_gateways' ), 1, 1 );
 	}
 
@@ -58,33 +57,6 @@ class wc4bp_Woocommerce {
 			return $default;
 		}
 	}
-
-
-	public function wc4bp_get_view_subscription_url( $view_subscription_url, $id ) {
-		global $bp;
-		$c_action                   = $bp->current_action;
-		$current_user               = wp_get_current_user();
-		$userdata                   = get_userdata( $current_user->ID );
-		$link                       = $view_subscription_url;
-		$my_account_page_id         = get_option( 'woocommerce_myaccount_page_id' );
-		$woo_my                     = get_post( $my_account_page_id );
-		$wc4bp_subscriptions_active = is_plugin_active( 'wc4bp-subscriptions/wc4bp-subscriptions.php' );
-
-		//If the wc4bp_subscriptions add-on is disable then the url of the subscription will go to
-		//woocommerce page
-		if ( ! $wc4bp_subscriptions_active ) {
-			if ( $c_action === 'wc4pb_subscriptions' ) {
-				$link = get_bloginfo( 'url' ) . '/' . $woo_my->post_name . '/view-subscription/' . $id;
-			}
-			if ( $c_action === 'wc4pb_orders' ) {
-
-				$link = get_bloginfo( 'url' ) . '/' . $woo_my->post_name . '/view-subscription/' . $id;
-			}
-		}
-
-		return $link;
-	}
-
 
 	public function wc4bp_woocommerce_is_account_page__premium_only( $is_account_page ) {
 		$default = $is_account_page;
