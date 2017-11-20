81fe4d699ac92d7119fbc96c3d0c8015436cc9b9
Guillermo Figueroa Mesa <gfirem@gmail.com>
2017-11-19 21:14:59 -0300

    Fixing the text error for free version.
    https://github.com/Themekraft/wc4bp/issues/149

--- admin/views/main/html_admin_my_account_tabs.php
+++ admin/views/main/html_admin_my_account_tabs.php
@@ -6,6 +6,6 @@ foreach ( WC4BP_MyAccount::get_available_endpoints() as $end_point_key => $end_p
 	if ( isset( $wc4bp_options[ 'wc4bp_endpoint_' . $end_point_key ] ) ) {
 		$tab_select = $wc4bp_options[ 'wc4bp_endpoint_' . $end_point_key ];
 	}
-	$text = ( $this->is_free ) ? sprintf( 'Turn on %s tab.', $end_point_name ) : sprintf( 'Turn off %s tab.', $end_point_name );
+	$text = sprintf( 'Turn off %s tab.', $end_point_name );
 	echo "<p " . $this->disable_class_tag( 'p' ) . "><label><input " . $this->disable_input_tag( 'checkbox' ) . " name='wc4bp_options[wc4bp_endpoint_" . $end_point_key . "]' type='checkbox' value='1' " . checked( $tab_select, 1, false ) . " /> <b>" . $text . "</b></label></p>";
 }
