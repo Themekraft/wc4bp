1b37863ad3468b3f0356373335ddfc78aebdd88c
Guillermo Figueroa Mesa <gfirem@gmail.com>
2017-12-03 20:25:30 -0300

    New Version 3.0.14.1

--- wc4bp-basic-integration.php
+++ wc4bp-basic-integration.php
@@ -5,7 +5,7 @@
  * Description: Integrates a WooCommerce installation with a BuddyPress social network
  * Author: ThemeKraft
  * Author URI: https://themekraft.com/products/woocommerce-buddypress-integration/
- * Version: 3.0.14
+ * Version: 3.0.14.1
  * Licence: GPLv3
  * Text Domain: wc4bp
  * Domain Path: /languages
@@ -48,7 +48,7 @@ class WC4BP_Loader {
 	/**
 	 * The plugin version
 	 */
-	const VERSION = '3.0.14';
+	const VERSION = '3.0.14.1';
 
 	/**
 	 * Minimum required WP version
