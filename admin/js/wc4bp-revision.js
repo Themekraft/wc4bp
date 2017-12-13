7b665330c73f7e1c9aa4e7db111098c36b47c710
Guillermo Figueroa Mesa <gfirem@gmail.com>
2017-12-13 20:56:30 -0300

    Adding the class to handle the ask to revision. It handle the reminder for 5 days.

---
+++ admin/js/wc4bp-revision.js
@@ -0,0 +1,41 @@
+function wc4bp_revision() {
+    function call_for_revision(action, container) {
+        if (action === 'review') {
+            window.open('https://wordpress.org/support/plugin/wc4bp/reviews/', '_blank');
+        }
+        jQuery.ajax({
+            type: 'POST', url: wc4bp_admin_revision_js.ajaxurl,
+            data: {
+                action: 'wc4bp_revision_' + action,
+                nonce: wc4bp_admin_revision_js.nonce,
+                trigger: action
+            },
+            success: function () {
+                container.hide();
+            }
+        });
+    }
+
+    return {
+        init: function () {
+            if (jQuery('#wc4bp-ask-revision').length > 0 && jQuery('button.review-notice-dismiss').length > 0) {
+                // Bind event handlers for form Settings page
+                this.attachEvents();
+            }
+        },
+        attachEvents: function () {
+            jQuery('button.review-notice-dismiss').each(function () {
+                var button = jQuery(this);
+                button.click(function () {
+                    var item = jQuery(this);
+                    call_for_revision(item.data('action'), jQuery('#wc4bp-ask-revision'));
+                });
+            });
+        }
+    }
+}
+
+var wc4bp_revision_action = wc4bp_revision();
+jQuery(document).ready(function () {
+    wc4bp_revision_action.init();
+});
---
+++ admin/views/html_admin_revision.php
@@ -0,0 +1,19 @@
+<div class="notice review-notice active" id="wc4bp-ask-revision" style="">
+    <div class="review-notice-logo"><span class="dashicons dashicons-chart-line"></span></div>
+    <div class="review-notice-message">
+        <strong>WC4BP -> WooCommerce BuddyPress Integration</strong>  <br>
+        Happy we could help you out! <img draggable="false" class="emoji" alt="🎉" src="https://s.w.org/images/core/emoji/2.3/svg/1f389.svg"> Could you please do me a BIG favor and give it a 5-star rating on WordPress? Just to help us spread the word and boost our motivation.<br/>
+        ~ <a target="_blank" href="http://themekraft.com/">ThemeKraft Team</a>
+    </div>
+    <div class="review-notice-cta">
+        <button class="review-notice-dismiss" data-action="review">
+            Ok, you deserve it
+        </button>
+        <button class="review-notice-dismiss" data-action="later">
+            Nope, maybe later
+        </button>
+        <button class="review-notice-dismiss" data-action="already">
+            I already did
+        </button>
+    </div>
+</div>
