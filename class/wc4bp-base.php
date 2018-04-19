6b72960b3e98dd8ebd035d7460538c3badaac359
Guillermo <gfirem@gmail.com>
2018-04-19 11:02:19 -0300

    * Enabling trials

--- class/wc4bp-base.php
+++ class/wc4bp-base.php
@@ -12,12 +12,13 @@ class wc4bp_base {
 	public $is_start;
 	public $is_professional;
 	public $is_premium_only;
-
+	public $is_trial;
+	
 	static $starter_plan_id = 'starter';
 	static $professional_plan_id = 'professional';
 	static $business_plan_id = 'business';
 	private $plan;
-
+	
 	public function __construct( $debug = false ) {
 		//Comment the next line to disable the forced debug
 //		$debug       = true;
@@ -28,10 +29,11 @@ class wc4bp_base {
 			$this->is_start        = WC4BP_Loader::getFreemius()->is_plan( self::$starter_plan_id );
 			$this->is_professional = WC4BP_Loader::getFreemius()->is_plan( self::$professional_plan_id );
 			$this->is_premium_only = WC4BP_Loader::getFreemius()->is__premium_only();
+			$this->is_trial        = WC4BP_Loader::getFreemius()->is_trial();
 
 			return;
 		} else if ( ! is_array( $debug ) ) {
-			$debug = array( 'is_paying' => false, 'is_free_plan' => true, 'starter' => false, 'professional' => false, 'is_premium_only' => false ); //Free
+			$debug = array( 'is_paying' => false, 'is_free_plan' => true, 'starter' => false, 'professional' => false, 'is_premium_only' => false , 'trial' => false ); //Free
 //			$debug = array( 'is_paying' => true, 'is_free_plan' => false, 'starter' => true, 'professional' => false, 'is_premium_only' => true ); //Starter
 //			$debug = array( 'is_paying' => true, 'is_free_plan' => false, 'starter' => false, 'professional' => true, 'is_premium_only' => true );//Professional
 		}
@@ -40,34 +42,39 @@ class wc4bp_base {
 		$this->is_start        = $debug['starter'];
 		$this->is_professional = $debug['professional'];
 		$this->is_premium_only = $debug['is_premium_only'];
+		$this->is_trial        = $debug['trial'];
 
 		//Set the fake plan
 		$this->plan = self::$starter_plan_id;
 	}
 
 	public function disable_class_tag( $tag, $plan = 'professional', $force = false ) {
-		if ( $force || ( ! $this->is_paying || $this->is_free || ! $this->is_plan( $plan ) ) ) {
-			switch ( $tag ) {
-				default:
-					$class = 'wc4bp-disabled';
+		if ( !$this->is_trial ) {
+			if ( $force || ( ! $this->is_paying || $this->is_free || ! $this->is_plan( $plan ) ) ) {
+				switch ( $tag ) {
+					default:
+						$class = 'wc4bp-disabled';
+				}
+				
+				return 'class="' . $class . '"';
 			}
-
-			return 'class="' . $class . '"';
 		}
 
 		return '';
 	}
 
 	public function disable_input_tag( $type, $plan = 'professional', $force = false ) {
-		if ( $force || ( ! $this->is_paying || $this->is_free || ! $this->is_plan( $plan ) ) ) {
-			switch ( $type ) {
-				case 'button':
-					$attr = 'disabled';
-					break;
-				default:
-					$attr = 'disabled="disabled"';
+		if ( !$this->is_trial ) {
+			if ( $force || ( ! $this->is_paying || $this->is_free || ! $this->is_plan( $plan ) ) ) {
+				switch ( $type ) {
+					case 'button':
+						$attr = 'disabled';
+						break;
+					default:
+						$attr = 'disabled="disabled"';
+				}
 			}
-
+			
 			return $attr;
 		}
 
@@ -78,14 +85,14 @@ class wc4bp_base {
 		if ( $this->debug ) {
 			$result = ( $this->plan == $plan_id );
 		} else {
-			$result = WC4BP_Loader::getFreemius()->is_plan( $plan_id );
+			$result = WC4BP_Loader::getFreemius()->is_plan_or_trial( $plan_id );
 		}
 
 		return $result;
 	}
 
 	public function needs_upgrade() {
-		return ( $this->is_free || $this->is_start ) && ! $this->is_professional;
+		return ( $this->is_free || $this->is_start || $this->is_trial ) && ! $this->is_professional;
 	}
 
 
