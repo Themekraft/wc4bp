939dc10d2ffb88bbd1354535e60ac364da3b13e4
Garrett Hyder <garrett@eclipse3sixty.com>
2018-06-27 12:57:52 -0700

    String fixes
    Capitalize C in WooCommerce and add -party to 3rd-party

--- admin/views/main/html_admin_shop_tabs.php
+++ admin/views/main/html_admin_shop_tabs.php
@@ -3,7 +3,7 @@
     <label>
         <input <?php echo $this->disable_input_tag( 'checkbox', wc4bp_base::$starter_plan_id ); ?> name='wc4bp_options[tab_activity_disabled]' type='checkbox' value='1' <?php checked( $tab_activity_disabled, 1 ); ?> />
         <b><?php _e( 'Turn off Shop.', 'wc4bp' ); ?></b>
-		<?php _e( 'Disable the BuddyPress Shop Tab and Woocommerce My Account will work normally.', 'wc4bp' ); ?>
+		<?php _e( 'Disable the BuddyPress Shop Tab and WooCommerce My Account will work normally.', 'wc4bp' ); ?>
     </label>
 </p>
 <p <?php /** @var wc4bp_admin $this */
@@ -17,13 +17,13 @@ echo $this->disable_class_tag( 'p', wc4bp_base::$starter_plan_id ); ?>>
 <p <?php echo $this->disable_class_tag( 'p', wc4bp_base::$starter_plan_id ); ?>>
     <label>
         <input <?php echo $this->disable_input_tag( 'checkbox', wc4bp_base::$starter_plan_id ); ?> name='wc4bp_options[tab_my_account_disabled]' type='checkbox' value='1' <?php checked( $tab_my_account_disabled, 1 ); ?> />
-        <b><?php _e( 'Turn off \'Woocommerce My Account\' redirection.', 'wc4bp' ); ?></b>&nbsp; <?php _e( 'This option is useful when you have problem with 3rd Woocommerce plugins. It disable only the My Account redirection, not the sub tabs url.', 'wc4bp' ); ?>
+        <b><?php _e( 'Turn off \'WooCommerce My Account\' redirection.', 'wc4bp' ); ?></b>&nbsp; <?php _e( 'This option is useful when you have problem with 3rd-party WooCommerce plugins. It disable only the My Account redirection, not the sub tabs url.', 'wc4bp' ); ?>
     </label>
 </p>
 <p <?php echo $this->disable_class_tag( 'p', wc4bp_base::$starter_plan_id ); ?>>
     <label>
         <input <?php echo $this->disable_input_tag( 'checkbox', wc4bp_base::$starter_plan_id ); ?> name='wc4bp_options[disable_woo_profile_override]' type='checkbox' value='1' <?php checked( $disable_woo_profile_override, 1 ); ?> />
-        <b><?php _e( 'Turn off \'Woocommerce User Profile \' override.', 'wc4bp' ); ?></b>&nbsp; <?php _e( 'This option is useful to stop Woocommerce override User Profile First Name, Last Name and Display Name.', 'wc4bp' ); ?>
+        <b><?php _e( 'Turn off \'WooCommerce User Profile \' override.', 'wc4bp' ); ?></b>&nbsp; <?php _e( 'This option is useful to stop WooCommerce override User Profile First Name, Last Name and Display Name.', 'wc4bp' ); ?>
     </label>
 </p>
 <p <?php echo $this->disable_class_tag( 'p' ); ?>>
