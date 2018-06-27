f762f9e19369b757de775c04694e00f8ab290829
Garrett Hyder <garrett@eclipse3sixty.com>
2018-06-27 13:00:15 -0700

    Grammar Fix

--- admin/views/main/html_admin_shop_tabs.php
+++ admin/views/main/html_admin_shop_tabs.php
@@ -17,7 +17,7 @@ echo $this->disable_class_tag( 'p', wc4bp_base::$starter_plan_id ); ?>>
 <p <?php echo $this->disable_class_tag( 'p', wc4bp_base::$starter_plan_id ); ?>>
     <label>
         <input <?php echo $this->disable_input_tag( 'checkbox', wc4bp_base::$starter_plan_id ); ?> name='wc4bp_options[tab_my_account_disabled]' type='checkbox' value='1' <?php checked( $tab_my_account_disabled, 1 ); ?> />
-        <b><?php _e( 'Turn off \'WooCommerce My Account\' redirection.', 'wc4bp' ); ?></b>&nbsp; <?php _e( 'This option is useful when you have problem with 3rd-party WooCommerce plugins. It disable only the My Account redirection, not the sub tabs url.', 'wc4bp' ); ?>
+        <b><?php _e( 'Turn off \'WooCommerce My Account\' redirection.', 'wc4bp' ); ?></b>&nbsp; <?php _e( 'This option is useful when you have problems with 3rd-party WooCommerce plugins. It disables only the My Account redirection, not the sub tabs url.', 'wc4bp' ); ?>
     </label>
 </p>
 <p <?php echo $this->disable_class_tag( 'p', wc4bp_base::$starter_plan_id ); ?>>
