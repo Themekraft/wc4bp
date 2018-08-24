3f8cc559098f0fcdc1aa6ebabeedc649733a0ef3
Guillermo Figueroa <gfirem@gmail.com>
2018-08-24 11:47:24 -0300

    Adding the option to handle the tabs names

--- admin/views/main/html_admin_my_account_tabs.php
+++ admin/views/main/html_admin_my_account_tabs.php
@@ -6,5 +6,5 @@ foreach ( $tabs_array as $end_point_key => $end_point_name ) {
 	$position   = $end_point_name['position'];
 
 	$text = sprintf( __( 'Turn off %s tab.', 'wc4bp' ), $end_point_name['label'] );
-	echo "<p " . $this->disable_class_tag( 'p' ) . "><label><span class=\"dashicons dashicons-sort\"></span><input " . $this->disable_input_tag( 'checkbox' ) . " name='" . $end_point_name['name'] . "' type='checkbox' value='1' " . checked( $tab_select, 1, false ) . " /> <input class='wc4bp-tabs-position' type='hidden' name='" . $end_point_name['name_position'] . "' value='" . $position . "'><b>" . $text . "</b></label></p>";
+	echo "<p " . $this->disable_class_tag( 'p' ) . "><label><span class=\"dashicons dashicons-sort\"></span><input " . $this->disable_input_tag( 'checkbox' ) . " name='" . $end_point_name['name'] . "' type='checkbox' value='1' " . checked( $tab_select, 1, false ) . " /> <input class='wc4bp-tabs-position' type='hidden' name='" . $end_point_name['name_position'] . "' value='" . $position . "'><b>" . $text . "</b></label> Or set tab's name <input type='text' value='".$end_point_name['label']."'></p>";
 }
