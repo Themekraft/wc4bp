ee5ec0d45cbf882aeb3a879cc4dde52c3667f212
Guillermo Figueroa Mesa (gfirem) <gfirem@gmail.com>
2019-06-04 12:20:28 -0300

    Fix the 404 my account pages for the free version

--- class/wc4bp-woocommerce.php
+++ class/wc4bp-woocommerce.php
@@ -23,10 +23,8 @@ class wc4bp_Woocommerce {
 		// Check if we are on checkout in profile
 		if ( ! isset( $this->wc4bp_options['tab_activity_disabled'] ) ) {
 			add_filter( 'woocommerce_is_checkout', array( $this, 'wc4bp_woocommerce_is_checkout' ) );
-			if ( WC4BP_Loader::getFreemius()->is_plan_or_trial__premium_only( wc4bp_base::$professional_plan_id ) ) {
-				// Check if we are on the my account page in profile
-				add_filter( 'woocommerce_is_account_page', array( $this, 'wc4bp_woocommerce_is_account_page__premium_only' ) );
-			}
+			// Check if we are on the my account page in profile
+			add_filter( 'woocommerce_is_account_page', array( $this, 'wc4bp_woocommerce_is_account_page' ) );
 			if ( ! isset( $this->wc4bp_options['tab_my_account_disabled'] ) ) {
 				add_filter( 'woocommerce_get_endpoint_url', array( $this, 'endpoint_url' ), 1, 4 );
 				add_filter( 'woocommerce_available_payment_gateways', array( $this, 'available_payment_gateways' ), 1, 1 );
@@ -93,7 +91,7 @@ class wc4bp_Woocommerce {
 		}
 	}
 
-	public function wc4bp_woocommerce_is_account_page__premium_only( $is_account_page ) {
+	public function wc4bp_woocommerce_is_account_page( $is_account_page ) {
 		$default = $is_account_page;
 		try {
 			if ( is_user_logged_in() ) {
