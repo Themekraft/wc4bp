fe2d3e569da2a0644fd03123d92ea7e205bd301b
Romeli Rodriguez <romelirodriguez@gmail.com>
2021-04-19 17:20:47 -0400

    Fixing the 404 error when all items are removed from checkout

--- class/wc4bp-redefine-functions.php
+++ class/wc4bp-redefine-functions.php
@@ -77,3 +77,37 @@ function wc4bp_filter_wc_checkout_item_remove( $product_name, $cart_item, $cart_
 		return $product_name;
     }
 }
+
+/**
+ * If there are no products in the checkout then, go to the cart   
+ */ 
+
+add_action( 'wp_loaded', 'wc4bp_redirection_checkout', 99 );
+
+function wc4bp_redirection_checkout(){
+
+	global $woocommerce, $bp;
+	$is_current_component = bp_is_current_component( wc4bp_Manager::get_shop_slug() );
+
+	if (is_checkout() && $is_current_component == 1 ) {
+	
+		if( $woocommerce -> cart -> cart_contents_count == 0 ){
+			wp_safe_redirect( wc_get_cart_url() );
+			exit;
+		}
+	}
+
+	do_action( 'wc4bp_redirection_check' );
+}
+
+/**
+ * Reload the cart page to remove the checkout tab  
+ */ 
+
+add_action( 'wp_enqueue_scripts', 'wc4bp_url_cart_refresh' );
+
+function wc4bp_url_cart_refresh() {
+    if( is_cart() ){
+    	wp_enqueue_script( 'wc4bp-checkout-refresh-page', dirname(plugin_dir_url(__FILE__)) . '/admin/js/wc4bp-cart-page-reload.js', array('jquery'), '1.0.0', true );
+    }
+}
