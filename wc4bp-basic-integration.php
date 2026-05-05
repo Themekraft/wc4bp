70f4f6a445e3f39aaa44f79b39a21f17da362f4d
Camilo Luna <camilo@alunizar.co>
2026-05-04 19:18:58 -0700

    build(beta): version 3.6.0-beta.1

--- wc4bp-basic-integration.php
+++ wc4bp-basic-integration.php
@@ -5,7 +5,7 @@
  * Description: Integrates WooCommerce with a BuddyPress social network
  * Author: ThemeKraft
  * Author URI: https://themekraft.com/products/woocommerce-buddypress-integration/
- * Version: 3.6.0
+ * Version: 3.6.0-beta.1
  * Licence: GPLv3
  * Text Domain: wc4bp
  * Domain Path: /languages
@@ -46,7 +46,7 @@ if ( ! class_exists( 'WC4BP_Loader' ) ) {
 		/**
 		 * The plugin version
 		 */
-		const VERSION = '3.6.0';
+		const VERSION = '3.6.0-beta.1';
 
 		/**
 		 * Minimum required WP version
